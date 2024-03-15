8f0eff0704dd4f793ba94608932ff7f3392bf80f
Vaibhav Zodge <zodgevaibhav@gmail.com>
2024-03-15 22:42:09 +0530

    Java formatter added

--- module2/auth-service/src/main/java/org/dnyanyog/service/LoginServiceImpl.java
+++ module2/auth-service/src/main/java/org/dnyanyog/service/LoginServiceImpl.java
@@ -1,7 +1,6 @@
 package org.dnyanyog.service;
 
 import java.util.List;
-
 import org.dnyanyog.dto.LoginRequest;
 import org.dnyanyog.dto.LoginResponse;
 import org.dnyanyog.entity.Users;
@@ -12,26 +11,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginServiceImpl implements LoginService {
 
-	@Autowired
-	UsersRepository userRepo;
+  @Autowired UsersRepository userRepo;
 
-	public LoginResponse validateUser(LoginRequest loginRequest) {
+  public LoginResponse validateUser(LoginRequest loginRequest) {
 
-		LoginResponse response = new LoginResponse();
+    LoginResponse response = new LoginResponse();
 
-		List<Users> liUser = userRepo
-				.findByUsernameAndPassword(
-						loginRequest.getUsername(), 
-						loginRequest.getPassword()
-						);
+    List<Users> liUser =
+        userRepo.findByUsernameAndPassword(loginRequest.getUsername(), loginRequest.getPassword());
 
-		if (liUser.size() == 1) {
-			response.setStatus("Success");
-			response.setMessage("Login successful");
-		} else {
-			response.setStatus("Fail");
-			response.setMessage("Login failed");
-		}
-		return response;
-	}
+    if (liUser.size() == 1) {
+      response.setStatus("Success");
+      response.setMessage("Login successful");
+    } else {
+      response.setStatus("Fail");
+      response.setMessage("Login failed");
+    }
+    return response;
+  }
 }
